d25ca874e9ef69032a00e9dffa7b921d9feb72eb
Jake Colledge <jakecolledgework@gmail.com>
2022-02-11 18:18:41 +1030

    added mutations.js

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,41 @@
+import { gql } from '@apollo/client'
+
+export const LOGIN = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+            username
+            email
+            password
+            stylist
+            }
+        }
+    }
+`
+
+export const ADD_USERS = gql`
+    mutation AddUser($username: String!, $email: String!, $password: String!, $stylist: Boolean!) {
+        addUser(username: $username, email: $email, password: $password, stylist: $stylist) {
+            token
+            user {
+            username
+            email
+            password
+            stylist
+            }
+        }
+    }
+`
+
+export const CREATE_APPOINTMENT = gql`
+    mutation CreateAppointments($name: String!, $userId: String!, $start: String!, $end: String!) {
+        createAppointment(name: $name, userId: $userId, start: $start, end: $end) {
+            _id
+            userId
+            name
+            start
+            end
+        }
+    }
+`
